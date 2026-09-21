ab45e1b83f00f7e0f333a65465f6532e8b0679dd
Bustamante Danilo <daniibustamantee@gmail.com>
2026-09-21 20:28:01 -0300

    Historial de entregas con datos reales del backend

--- src/app/historial.tsx
+++ src/app/historial.tsx
@@ -1,16 +1,44 @@
-import { View, Text, StyleSheet } from "react-native";
+import PedidoCard from "@/components/PedidoCard";
+import { fetchAuth } from "@/services/auth";
+import { useEffect, useState } from "react";
+import { ScrollView, StyleSheet, Text } from "react-native";
 
-export default function historial() {
+
+
+export default function Historial() {
+
+  const [envios, setEnvios] = useState<any[]>([]);
+
+  useEffect(() => {
+    async function cargar() {
+      const res = await fetchAuth("/envios/mis-envios");
+      const data = await res.json();
+      setEnvios(data);
+
+    }
+    cargar();
+
+  }, []);
+
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.titulo}>Historial de entregas</Text>
-      <Text>✅ Pedido #1 - Entregado</Text>
-      <Text>✅ Pedido #2 - Entregado</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <Text style={styles.titulo}>Historial de Pedidos</Text>
+      {envios.filter((envio) => envio.resultado_entrega).map((envio) => (
+        <PedidoCard
+          key={envio.id_envio}
+          nroPedido={envio.nro_pedido}
+          Cliente={envio.contacto_receptor}
+          estado={envio.resultado_entrega}
+          fecha={envio.fecha_entrega_real}
+        />
+      ))}
+    </ScrollView>
+
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, padding: 20 },
+  container: { flex: 1, padding: 20, backgroundColor: "#f9f9f9" },
   titulo: { fontSize: 22, fontWeight: "bold", marginBottom: 15 }
 });
